e3bd85fa09a0c559253736cadc0afe497cbe3bcf
addamsv <googolcalculator@gmail.com>
2024-10-31 21:38:46 +0300

    feat: spring profiles & fix: 404

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,9 +1,11 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ActiveProfiles;
 // import org.springframework.boot.test.context.SpringBootTest;
 
 //@SpringBootTest
+@ActiveProfiles(value = "development")
 class DemoApplicationTests {
 
 	@Test
